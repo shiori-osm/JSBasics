1b2aff3c09dc73e3d9289aadd9202e5bdebf17d2
しおり.大隅 <metalgear.shiorid@gmail.com>
2021-07-07 08:01:19 +0900

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const luck = 4;
+
+switch(luck){
+  case 1:
+    console.log("大吉だよ★");
+    break
+  case 2:
+    console.log("中吉");
+    break
+  case 3:
+    console.log("吉");
+    break
+  case 4:
+    console.log("末吉");
+    break
+  case 5:
+    console.log("凶");
+    break
+  case 6:
+    console.log("大凶");
+    break
+}
